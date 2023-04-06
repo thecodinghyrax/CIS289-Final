b1bb9b7ac4afc8a624dfbbf79f7c8b5eecebf1a3
Drew Crawford <thecodinghyrax@gmail.com>
2023-04-05 19:51:51 -0500

    add main

---
+++ main.py
@@ -0,0 +1,5 @@
+# Use Scipy filter module. Use either Bessel or Butterworth filter. Cut off should be 300 to 4000hz
+# Research - Generate a spectogram to find the part of the recording that has the lowest average frequency response. 
+    # subtract that from all other points in the audio sample
+# Clip the audio
+

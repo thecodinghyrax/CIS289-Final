b824296e45f2b3b170b52b76f34c33b34464a5ad
Drew Crawford <thecodinghyrax@gmail.com>
2023-04-14 15:32:33 -0500

    Created db tables

--- CIS289_Final/part_tracker/models.py
+++ CIS289_Final/part_tracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Catagory(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Version(models.Model):
+    brand = models.CharField(max_length=50)
+    number = models.CharField(max_length=100)
+    image_location = models.CharField(max_length=100, blank=True)
+    
+class Component(models.Model):
+    catagory = models.ForeignKey(Catagory, on_delete=models.CASCADE)
+    version = models.ForeignKey(Version, on_delete=models.CASCADE)
+    price = models.IntegerField(default=0)
+    date = models.DateTimeField("Price Date")
+    retailer = models.CharField(max_length=50)
+    link = models.CharField(max_length=2000)
+    
+    
+    
+    

c93a79fb40646855259cccd230b1af0f243ff3d2
Drew Crawford <thecodinghyrax@gmail.com>
2023-04-14 21:12:08 -0500

    outline is in place

--- CIS289_Final/part_tracker/views.py
+++ CIS289_Final/part_tracker/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Hello World!')
+def index(request):
+    catagories = ["CPU", "GPU", "case", "PSU", "mother board", "storage", "OS", "RAM"]
+    context = {"catagories": catagories}
+    return render(request, "part_tracker/index.html", context)
